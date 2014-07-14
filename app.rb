6d0a5d11f7595ccdc952c5cc55f0e675234fd08f
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-07-14 16:19:05 +0200

    basic API in front of ledgerrb lib

--- app.rb
+++ app.rb
@@ -1,8 +1,46 @@
 # encoding: utf-8
 
-require 'sinatra'
-require 'sinatra/reloader'
+require 'json'
 
-get '/' do
-  'OH HAI!'
+Bundler.require( :default, :development ) # require tout les gems définis dans Gemfile
+
+require_relative './lib/ledger'
+
+# Sinatra app serving API
+class LedgerRbApp < Sinatra::Base
+  helpers Sinatra::Param
+
+  get '/' do
+    'OH HAI!'
+  end
+
+  get '/api/ledger/accounts/?' do
+    content_type :json
+
+    Ledger.accounts.to_json
+  end
+
+  get '/api/ledger/accounts/depth/:depth' do
+    content_type :json
+    param :depth, Integer
+
+    Ledger.accounts( params[ :depth ] ).to_json
+  end
+
+  get '/api/ledger/monthly/?' do
+    content_type :json
+    param :categories, Array
+
+    params[ :categories ].map do |category|
+      { category: category,
+        data: Ledger.monthly_register( category ).to_json }
+    end
+  end
+
+  get '/api/ledger/version/?' do
+    content_type :json
+
+    Ledger.version
+  end
+
 end
