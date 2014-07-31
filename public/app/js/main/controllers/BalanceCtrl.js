44c3b1adc51f0cd792332f666d2ba9540064dd2a
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-07-31 18:13:24 +0200

    income are negative

--- public/app/js/main/controllers/BalanceCtrl.js
+++ public/app/js/main/controllers/BalanceCtrl.js
@@ -228,7 +228,7 @@ app.controller( 'BalanceCtrl',
 				     + '&categories=Income' )
 			      .then( function( response ) {
 				  $scope.balance.income = _(response.data).sortBy( function( account ) {
-				      return 1 / account.amount;
+				      return account.amount;
 				  } );
 				  $scope.balance.income_total = _(response.data).reduce( function( memo, account ){ return memo + account.amount; }, 0 );
 			      } );
