a8fd3f94b748548d1cae0ff583d6d437f79c8adf
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-01 13:57:43 +0200

    removed useless division between core/ and main/

---
+++ public/app/js/state.js
@@ -0,0 +1,29 @@
+// Sub-application/main Level State
+app.config(['$stateProvider', function($stateProvider) {
+
+    $stateProvider
+	.state('app.balance', {
+	    url: '/balance',
+	    templateUrl: 'js/templates/balance.tpl.html',
+	    controller: 'BalanceCtrl'
+	})
+	.state('app', {
+	    url: '',
+	    controller: 'AppCtrl',
+	    views: {
+		'navbar': {
+		    templateUrl: 'js/templates/navbar.tpl.html',
+		    controller: 'NavbarCtrl'
+		},
+		'main': {
+		    templateUrl: 'js/templates/main.tpl.html'
+		}
+	    }
+	})
+	.state('404', {
+	    url: '/404',
+	    templateUrl: 'js/templates/404.tpl.html',
+	    controller: 'AppCtrl'
+	});
+
+}]);
